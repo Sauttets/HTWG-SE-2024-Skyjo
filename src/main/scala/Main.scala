f7dd984be4f0472111b4dc0c2575e7019e8f7602
Matze-Katze <Matthias.leray@htwg-konstanz.de>
2024-04-19 20:24:42 +0200

    extracted TUI

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 import scala.util.Random
-
+import TUI.*
+class Card(value:Int,open:Boolean=false){
+  def isOpen():Boolean=open
+}
 object CardMatrix {
 
   val rows = 3
@@ -7,43 +10,17 @@ object CardMatrix {
   val playerCount = 2
   val leaderboard = Array.fill[Int](playerCount)(0)
   val initialValue = 99
+  val cardValues=List(5,10,15,10,10,10,10,10,10,10,10,10,10,10,10)
+  val cardList=cardValues.zipWithIndex.map((value,ind)=>List.tabulate(value)(x=>Card(ind))).flatten
+  
+  def getRndmCards():List[Card]=
+    scala.util.Random.shuffle(cardList)
 
   def createRow(initialValue: Int, size: Int): List[Int] =
     List.fill(size)(initialValue)
 
   def createMatrix(initialValue: Int, rows: Int, cols: Int): List[List[Int]] =
     List.fill(rows)(createRow(initialValue, cols))
-
-  def padValue(value: Int): String =
-    if (value >= 0 && value < 10) "0" + value
-    else if (value == 99) "xx"
-    else value.toString
-
-  def formatRow(row: List[Int]): String = {
-    val formattedRow = row.map { elem => padValue(elem)}
-    "│ " + formattedRow.mkString(" │ ") + " │"
-  }
-
-  def printMatrix(matrix: List[List[Int]]): Unit = {
-    println("┌" + "────┬" * (matrix.head.length - 1) + "────┐")
-    matrix.zipWithIndex.foreach { case (row, index) =>
-      if (index == matrix.length - 1) {
-        println(formatRow(row))
-        println("└" + "────┴" * (matrix.head.length - 1) + "────┘")
-      } else {
-        println(formatRow(row))
-        println("├" + "────┼" * (matrix.head.length - 1) + "────┤")
-      }
-    }
-  }
-
-  def printCardStack(stack:Int = 99, thrown:Int = 99): Unit = {
-      println("Current card stack:")
-      println("  ┌────┐  ┌────┐" )
-      println(s" ┌┤ ${padValue(stack)} │  │ ${padValue(thrown)} │")
-      println(" │└───┬┘  └────┘")
-      println(" └────┘")
-  }
 
 }
 
