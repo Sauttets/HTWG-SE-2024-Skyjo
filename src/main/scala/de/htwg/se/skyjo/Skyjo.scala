c61a6fb817f313cf202ee52d0047f62b53a8ab66
sauttets <tobias@sautter.com>
2024-05-07 21:11:47 +0200

    fix

--- src/main/scala/de/htwg/se/skyjo/Skyjo.scala
+++ src/main/scala/de/htwg/se/skyjo/Skyjo.scala
@@ -5,25 +5,7 @@ import de.htwg.se.skyjo.aview._
 
 @main def main(): Unit =
   var table = new PlayerTable()
-  //println(table.getPlayerMatricesString())
-  //println(table.getCardStackString())
-  //table = table.drawFromStack()
-  //print(table.getTableString())
-  /*
-  var tableController = new TableController(table)
-  tableController.drawFromStack()
-  print(tableController.table.getTableString())
-
-  var move = new Move(true, true, 0, 0)
-  tableController.doMove(move)
-  print(tableController.table.getTableString())
-
-
-  move = new Move(true, false, 0, 1)
-  tableController.doMove(move)
-  print(tableController.table.getTableString())
-  */
-
   var tableController = new TableController(table)
   var tui = new TUI(tableController)
   tui.run
+
