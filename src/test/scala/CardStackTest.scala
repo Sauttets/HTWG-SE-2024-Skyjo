eccbb60e722f2f9b3784bf1a2e557feef8fb20eb
Matze-Katze <Matthias.leray@htwg-konstanz.de>
2024-05-09 14:21:16 +0200

    Card and CardStacktests

---
+++ src/test/scala/CardStackTest.scala
@@ -0,0 +1,61 @@
+import de.htwg.se.skyjo._
+import org.scalatest._
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.skyjo.model.Card
+import de.htwg.se.skyjo.model.Cardstack
+import org.scalatest.matchers.should._
+
+class CardStackTest extends AnyWordSpec with Matchers{ 
+ "A CardStack" when{
+    "created should have initial state" should{
+      "without parameters" in{
+        val stack=new Cardstack
+        stack.stackCard should not be null
+        stack.stackCard.opened shouldBe false
+      }
+      "with Cards" in{
+        val (a,b)=(new Card,new Card)
+        val stack=new Cardstack(a,b)
+        stack.stackCard should equal (a)
+        stack.trashCard should equal (b)
+      }
+    }
+    "drawn from Stack" should{
+      val stack=new Cardstack
+      val sCard=stack.stackCard
+      val tCard=stack.trashCard
+      "flip stackcard" in{
+        val drawnStack=stack.drawFromStack()
+        drawnStack.trashCard shouldEqual tCard
+        drawnStack.stackCard.value shouldBe sCard.value
+        drawnStack.stackCard.opened shouldBe true
+      }
+      "change stackCard" in{
+        val nStack=stack.newStackCard()
+        nStack.stackCard should not equal stack.stackCard
+        nStack.trashCard shouldEqual stack.trashCard
+      }
+    }
+    "drawn from trash" should{
+      val stack=new Cardstack
+      val sCard=stack.stackCard
+      val tCard=stack.trashCard
+      "give Trashcard" in{
+        val (tCard,cardStack)=stack.drawFromTrash()
+        stack.trashCard shouldEqual tCard
+        cardStack.trashCard should not equal (stack.trashCard)
+      }
+    }
+    "discarding" should{
+      val stack=new Cardstack
+      val sCard=stack.stackCard
+      "discard card" in{
+        val dCard=new Card
+        val dStack=stack.discard(dCard)
+        dStack.trashCard.value shouldEqual dCard.value
+        dStack.trashCard.opened shouldBe true
+        dStack.stackCard shouldEqual sCard
+      }
+    }
+  }
+}
